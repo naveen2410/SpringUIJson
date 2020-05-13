1c8c7b43f962d80d15d7eb757263aa622b5d89ad
Naveen <navegoya@in.ibm.com>
2020-05-13 14:05:47 +0530

    s

--- src/main/java/connplantsTest/connplantsTest/HomeController.java
+++ src/main/java/connplantsTest/connplantsTest/HomeController.java
@@ -47,7 +47,7 @@ public class HomeController {
 	
 	@GetMapping
 	public String index() {
-		return "redirect:/ajax";
+		return "redirect:/user";
 	}
 
 	@GetMapping("/test")
@@ -56,11 +56,11 @@ public class HomeController {
 		return "test";
 	}
 	
-	/*@GetMapping("/user")
+	@GetMapping("/user")
 	public String loginUser(Model model) {
 		// model.addAttribute("username", new SearchCriteria());
 		return "ajax";
-	}*/
+	}
 	
     @PostMapping("/api/search")
     public ResponseEntity<?> getSearchResultViaAjax(@ModelAttribute SearchCriteria search) {
